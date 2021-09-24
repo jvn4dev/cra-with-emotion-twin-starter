e178992268ed3df1cc005a6a9eac8159427dc3bb
intae kim <intaemon@intaeui-MacBookAir.local>
2021-09-24 11:51:07 +0900

    refactor: Button 컴포넌트 변경

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,42 +1,30 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import PropTypes from 'prop-types'; // ES6
-import tw, { css, theme } from 'twin.macro';
+import tw, { styled, css, theme } from 'twin.macro';
 
-const Button = ({ children, onClick, variant, isSmall }) => (
-  <button
-    type="submit"
-    onClick={onClick}
-    css={[
-      tw`px-8 py-2 rounded focus:outline-none transform duration-75`,
-      tw`hocus:(scale-105 text-yellow-400)`,
-      variant === 'primary' && tw`bg-black text-white border-black`, // ! 조건부 css 작성
-      variant === 'secondary' && [
-        css`
-          box-shadow: 0 0.1em 0 0 rgba(0, 0, 0, 0.25); // ! 커스텀 css 작성
-        `,
-        tw`border-2 border-yellow-600`,
-      ],
-      isSmall ? tw`text-sm` : tw`text-lg`,
-      tw`animate-wiggle`,
-      css`
-        color: ${theme`colors.white`}; // ! 커스텀 css + theme
-      `,
-    ]}>
-    {children}
-  </button>
-);
+const Button = styled.button(({ variant, isSmall }) => [
+  // The common button styles added with the tw import
+  tw`px-8 py-2 rounded focus:outline-none transform duration-75`,
 
-Button.defaultProps = {
-  variant: 'primary',
-  isSmall: false,
-  onClick: () => null,
-};
+  // Use the variant grouping feature to add variants to multiple classes
+  tw`hocus:(scale-105 text-yellow-400)`,
 
-Button.propTypes = {
-  children: PropTypes.string.isRequired,
-  variant: PropTypes.oneOf(['primary', 'secondary']),
-  isSmall: PropTypes.bool,
-  onClick: PropTypes.func,
-};
+  // Use props to conditionally style your components
+  variant === 'primary' && tw`bg-black text-white border-black`,
+
+  // Combine regular css with tailwind classes within backticks
+  variant === 'secondary' && [
+    css`
+      box-shadow: 0 0.1em 0 0 rgba(0, 0, 0, 0.25);
+    `,
+    tw`border-2 border-yellow-600`,
+  ],
+
+  // Conditional props can be used
+  isSmall ? tw`text-sm` : tw`text-lg`,
+
+  // The theme import can supply values from your tailwind.config.js
+  css`
+    color: ${theme`colors.white`};
+  `,
+]);
 
 export default Button;
